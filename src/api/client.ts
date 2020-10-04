10deb0f2d4112f698bbc892c3a89ea11c20fc9ce
delta-kor <deltakbot1@gmail.com>
2020-10-04 22:23:27 +0900

    Add api request

--- src/api/client.ts
+++ src/api/client.ts
@@ -8,20 +8,30 @@ export enum ApiPath {
 
 export default class ApiClient {
   private readonly baseUrl: string;
-  private readonly basicHeaders: HttpHeader;
+  private readonly headers: HttpHeader;
+  private readonly baseQuery: HttpQuery;
 
   constructor() {
     this.baseUrl = 'https://map.naver.com/v5/api';
-    this.basicHeaders = {
+    this.headers = {
       Accept: 'application/json, text/plain, */*',
       'Content-Type': 'application/json',
     };
+    this.baseQuery = {
+      lang: 'ko',
+      caller: 'pcweb',
+    };
   }
 
-  public async request<T>(path: ApiPath, query: HttpQuery): Promise<any> {}
+  public async request<T>(path: ApiPath, query: HttpQuery): Promise<T> {
+    const url = this.buildPath(path, query);
+    const response = await Axios.get(url, { headers: this.headers });
+    return response.data;
+  }
 
   private buildPath(path: ApiPath, query: HttpQuery): string {
-    const params = new URLSearchParams(query);
+    const urlQuery = Object.assign(query, this.baseQuery);
+    const params = new URLSearchParams(urlQuery);
     return `${this.baseUrl}/${path}?${params}`;
   }
 }
